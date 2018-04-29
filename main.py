3dd19c70b270e41324fd34ff5b8fb50c96df9321
octaltree <octaltree@gmail.com>
2018-04-30 01:21:49 +0900

    writing

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
+import os
+import os.path
+import dotenv
+import ccxt
 
 
 def main() -> int:
+    inited = init()
     return 0
 
+def init():
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    dotenv.load_dotenv(dotenv_path)
+    hitbtc2 = ccxt.hitbtc2({
+        'apiKey': os.environ.get('hitbtc2_key'),
+        'secret': os.environ.get('hitbtc2_secret')})
+    bitbank = ccxt.bitbank({
+        'apiKey': os.environ.get('bitbank_key'),
+        'secret': os.environ.get('bitbank_secret')})
+    return None
 
 if __name__ == "__main__":
     exit(main())
