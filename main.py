f2be4c12eac023f2635ca1c59650dfadfa450450
octaltree <octaltree@gmail.com>
2018-04-29 23:36:09 +0900

    add blank main

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+def main() -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
+
+# vim:fenc=utf-8 ff=unix ft=python ts=4 sw=4 sts=4 fdm=indent fdl=0 fdn=1:
+# vim: si et cinw=if,elif,else,for,while,try,except,finally,def,class:
