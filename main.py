3877fe7b2696f168cf42d1ae315001f956e1aea1
octaltree <octaltree@gmail.com>
2018-05-17 16:50:43 +0900

    threashold

--- main.py
+++ main.py
@@ -137,7 +137,7 @@ def attemptTrade(inited, capacity, value, production=False):
     # XRPの枚数で取引量を示す
     # 1: BASE2/BASE1, 2: ALT/BASE2, 3: ALT/BASE1
     # 1: JPY/BTC, 2: XRP/JPY, 3: XRP/BTC
-    thresholdS = 1.007
+    thresholdS = 1.005
     thresholdB = 1.003
     (ratioS, valS, pbjS, pbxS, phxS) = calcSellingTwice(
         bitbankJpy['bids'],
